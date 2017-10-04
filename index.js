3aa114e93511b610f77491a8f46a050d2e6f1bc4
Zia <fzia@newpark.com>
2017-10-04 10:05:35 -0500

    Open file in editor

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ app.on("ready",()=>{
     mainWindow = new BrowserWindow();
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
+    mainWindow.webContents.on("will-navigate",(event, url)=>{
+        event.preventDefault();
+        console.log(url);
+        mainWindow.webContents.send('open-file', url)
+    })
+
 })
--- renderer.js
+++ renderer.js
@@ -1,9 +1,23 @@
 const loader = require('monaco-loader')
+const {ipcRenderer}= require('electron')
+const fs = require("fs")
+
 loader().then((monaco)=>{
     const div= document.querySelector('#container')
     let editor = monaco.editor.create(div, {
         language:'javascript',
         theme: 'vs-dark',
         automaticLayout:true
+    })
+
+    ipcRenderer.on('open-file',(sender, url = '')=>{
+        url=url.slice(8);
+        console.log(url);
+
+        const filedata = fs.readFileSync(url, 'utf-8')
+        console.log(filedata)
+
+        const model = monaco.editor.createModel(filedata, 'javascript')
+        editor.setModel(model)
     })
 })
